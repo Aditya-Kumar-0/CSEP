e2be96ac777c2201a9eff90a3948ee65ce375ce3
Aditya-Kumar-0 <adityakumarak701786@gmail.com>
2026-09-08 22:09:22 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Frequencies in a Limited Array/frequencies-in-a-limited-array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    vector<int> frequencyCount(vector<int>& arr) {
+        // code here
+        int n = arr.size();
+        for(int i=0; i<n; i++){
+            arr[i]--;
+        }
+        
+        for(int i=0; i<n; i++){
+            arr[arr[i]%n]+=n;
+        }
+        
+        for(int  i=0; i<n; i++){
+            arr[i] = arr[i]/n;
+        }
+        return arr;
+    }
+};
