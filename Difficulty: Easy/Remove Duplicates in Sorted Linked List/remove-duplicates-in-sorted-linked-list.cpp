78de0dc33855e912fc42612e083b31696cad57c3
Aditya-Kumar-0 <adityakumarak701786@gmail.com>
2026-09-14 22:58:24 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Remove Duplicates in Sorted Linked List/remove-duplicates-in-sorted-linked-list.cpp
@@ -0,0 +1,33 @@
+/* Structure of linked list Node
+class Node {
+  public:
+    int data;
+    Node *next;
+
+    Node(int x) {
+        data = x;
+        next = nullptr;
+    }
+};
+*/
+class Solution {
+  public:
+    Node* removeDuplicates(Node* head) {
+        // code here
+        Node* curr = head->next;
+        Node* prev = head;
+        while(curr){
+            if(curr->data == prev->data){
+                prev->next = curr->next;
+                delete curr;
+                curr = prev->next;
+            }
+            else{
+                
+                prev = curr;
+                curr = curr->next;
+            }
+        }
+        return head;
+    }
+};
