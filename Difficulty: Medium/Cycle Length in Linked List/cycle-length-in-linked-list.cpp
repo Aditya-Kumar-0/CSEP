9d24f5c0164f92be5cb4f6740638324e42e2de48
Aditya-Kumar-0 <adityakumarak701786@gmail.com>
2026-09-15 20:16:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Cycle Length in Linked List/cycle-length-in-linked-list.cpp
@@ -0,0 +1,36 @@
+/* Structure of Linked List Node
+class Node {
+ public:
+    int data;
+    Node *next;
+    Node(int x) {
+        data = x;
+        next = nullptr;
+    }
+};*/
+
+class Solution {
+  public:
+    int lengthOfLoop(Node *head) {
+        // code here
+        Node* slow = head;
+        Node* fast = head;
+        
+        while(fast && fast->next){
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow==fast)
+            break;
+        }
+        if(fast == NULL || fast->next==NULL)
+        return 0;
+        
+        int count = 1;
+        slow = fast -> next;
+        while(slow!=fast){
+            count++;
+            slow = slow->next;
+        }
+        return count;
+    }
+};
