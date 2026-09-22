db22f47b39a60b0cd351105c4327df02240adee2
Aditya-Kumar-0 <adityakumarak701786@gmail.com>
2026-09-22 17:51:08 +0530

    Time: 2 ms (34.82%), Space: 10 MB (34.44%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string ans;
+        for(int i=0; i<s.size(); i++){
+            if(s[i]>='A' && s[i]<='Z'){
+                s[i]+=32;
+            }
+            if(s[i]>='a' && s[i]<='z'){
+                ans.push_back(s[i]);
+            }
+            else if(s[i]>='0' && s[i]<='9')
+            ans.push_back(s[i]);
+        }
+
+        int i=0, j=ans.size()-1;
+        while(i<=j){
+            if(ans[i]!=ans[j]){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+};
